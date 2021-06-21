bbe62939b51a20665e1e50bcc1472dacf2130b7b
Olybear9 <olybear9@gmail.com>
2021-06-20 20:20:54 -0500

    📝 chore: Fix formatting

--- src/protocol/offline.rs
+++ src/protocol/offline.rs
@@ -1,25 +1,25 @@
-use binary_utils::{ stream::*, IBufferWrite, IBufferRead };
 use super::{ IClientBound, IServerBound, Packet };
+use binary_utils::{stream::*, IBufferRead, IBufferWrite};
 
 pub enum OfflinePackets {
      UnconnectedPing = 0x01,
      OpenConnectRequest = 0x05,
-	OpenConnectReply = 0x06,
-	SessionInfo = 0x07,
-	SessionInfoReply = 0x08,
-	UnconnectedPong = 0x1c
+     OpenConnectReply = 0x06,
+     SessionInfo = 0x07,
+     SessionInfoReply = 0x08,
+     UnconnectedPong = 0x1c,
 }
 
 // Open Connection Reply One
 pub struct OpenConnectReply {
      server_id: i64,
      security: bool,
-     mtu: u16
+     mtu: u16,
 }
 
 impl IClientBound<OpenConnectReply> for OpenConnectReply {
      fn to(packet: OpenConnectReply) -> BinaryStream {
-          let mut stream = BinaryStream::new(vec!(0));
+          let mut stream = BinaryStream::new(vec![0]);
           stream.write_byte(OfflinePackets::OpenConnectReply as u16);
           stream.write_long(packet.server_id);
           stream.write_bool(packet.security);
