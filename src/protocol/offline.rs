d53c78886a07e3aded5512df8f4f91f7ba1fb93b
Olybear9 <olybear9@gmail.com>
2021-06-22 00:14:26 -0500

    🆕 feat: Add Session Info to Offline

--- src/protocol/offline.rs
+++ src/protocol/offline.rs
@@ -1,6 +1,8 @@
-use super::{ IClientBound, IServerBound, Packet };
-use crate::IPacketStreamWrite;
+#![allow(dead_code)]
+use super::{ IClientBound, IServerBound };
+use crate::{ IPacketStreamWrite, IPacketStreamRead };
 use binary_utils::{ stream::*, IBufferRead, IBufferWrite };
+// use std::net::{ IpAddr, SocketAddr };
 
 pub enum OfflinePackets {
      UnconnectedPing = 0x01,
@@ -11,7 +13,8 @@ pub enum OfflinePackets {
      UnconnectedPong = 0x1c,
 }
 
-// Open Connection Reply One
+/// Open Connection Reply
+/// Sent to the client when the server accepts a client.
 pub struct OpenConnectReply {
      server_id: i64,
      security: bool,
@@ -20,12 +23,48 @@ pub struct OpenConnectReply {
 
 impl IClientBound<OpenConnectReply> for OpenConnectReply {
      fn to(packet: OpenConnectReply) -> BinaryStream {
-          let mut stream = BinaryStream::new(vec![0]);
+          let mut stream = BinaryStream::new();
           stream.write_byte(OfflinePackets::OpenConnectReply as u16);
           stream.write_magic();
           stream.write_long(packet.server_id);
           stream.write_bool(packet.security);
           stream.write_short(packet.mtu);
           stream
+     }
+}
+
+/// A connection request from the client.
+pub struct OpenConnectRequest {
+     protocol: u16,
+     mtu_size: usize
+}
+
+impl IServerBound<OpenConnectRequest> for OpenConnectRequest {
+     fn from(mut s: BinaryStream) -> OpenConnectRequest {
+          let p = s.read_byte();
+          let mtu = s.get_length() + 1 + 28;
+          OpenConnectRequest {
+               protocol: p,
+               mtu_size: mtu
+          }
+     }
+}
+
+/// Session info, also known as Open Connect Request 2
+pub struct SessionInfo {
+     magic: Vec<u8>,
+     // address: SocketAddr,
+     mtu: usize,
+     client_id: i64
+}
+
+impl IServerBound<SessionInfo> for SessionInfo {
+     fn from(mut stream: BinaryStream) -> SessionInfo {
+          Self {
+               magic: stream.read_magic(),
+               // address: SocketAddr::from((0,0,0,0)),
+               mtu: stream.read_short() as usize,
+               client_id: stream.read_long()
+          }
      }
 }
