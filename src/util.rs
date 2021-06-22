5bfca421910041f1a0f86818c15e804dc358be90
Olybear9 <olybear9@gmail.com>
2021-06-22 00:14:26 -0500

    🆕 feat: Add Session Info to Offline

--- src/util.rs
+++ src/util.rs
@@ -1,12 +1,22 @@
-use binary_utils::{ stream::*, IBufferRead, IBufferWrite };
+use binary_utils::stream::*;
 
 // Raknet utilities
 pub trait IPacketStreamWrite {
      fn write_magic(&mut self);
 }
 
+pub trait IPacketStreamRead {
+     fn read_magic(&mut self) -> Vec<u8>;
+}
+
 impl IPacketStreamWrite for BinaryStream {
      fn write_magic(&mut self) {
 
+     }
+}
+
+impl IPacketStreamRead for BinaryStream {
+     fn read_magic(&mut self) -> Vec<u8> {
+          crate::MAGIC.to_vec()
      }
 }
