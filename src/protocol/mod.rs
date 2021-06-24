a4c7033fe8f3565edb4e4a6321fbf218f75c6270
Olybear9 <olybear9@gmail.com>
2021-06-23 19:54:06 -0500

    ✏ add docs

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -2,10 +2,14 @@ use binary_utils::BinaryStream;
 pub mod offline;
 pub mod online;
 
+/// Trait used when a packet should be **sent** to the client.
+/// AKA: This packet is being sent to the client.
 pub trait IClientBound<Pk = Packet> {
      fn to(packet: Pk) -> BinaryStream;
 }
 
+/// Trait used when a packet is being **recieved** from the client.
+/// AKA: This packet is being recieved from the client.
 pub trait IServerBound<Pk = Packet> {
      fn from(stream: BinaryStream) -> Pk;
 }
