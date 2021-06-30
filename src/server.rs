888579796a80a1be4638db5af2a5010f818308b2
Olybear9 <olybear9@gmail.com>
2021-06-30 15:57:56 -0500

    🆕 working server??

--- src/server.rs
+++ src/server.rs
@@ -10,15 +10,13 @@ pub const THREAD_QUIT: i64 = 982939013934;
 
 pub trait IRakServer {
      fn new(address: String, version: u8) -> Self;
-
-     fn start(&mut self);
+     fn start(&mut self) -> (&mut RakServer, Arc<RakEmitter>);
 }
 
 pub struct RakServer {
      address: String,
      version: u8,
-     connections: Vec<Connection>,
-     channel: RakEventListener,
+     connections: Vec<Connection>
 }
 
 impl IRakServer for RakServer {
@@ -26,54 +24,62 @@ impl IRakServer for RakServer {
           Self {
                version,
                address,
-               connections: Vec::new(),
-               channel: RakEventListener::new()
+               connections: Vec::new()
           }
      }
 
-     fn start(&mut self) {
-          let socket = UdpSocket::bind(self.address.parse::<SocketAddr>().unwrap());
-          let resource: Arc<UdpSocket> = Arc::new(socket.unwrap());
-          let res = Arc::clone(&resource);
-          let channels = (Arc::new(&self.channel), Arc::new(&self.channel)); // we'll use this across threads.
-
-          // ServerBound
-          thread::spawn(move || {
-               let mut buf = [0; 65535];
-               loop {
-                    let (len, rem) = match resource.as_ref().recv_from(&mut buf) {
-                         Ok(v) => v,
-                         Err(e) => {
-                              continue;
-                         }
-                    };
-
-                    let data = &buf[..len];
-                    channels.0.as_ref().broadcast(RakEvent::Recieve(rem, BinaryStream::init(&data.to_vec())));
-               }
-          });
-
-          // thing to get shit from server
-          let (sr, rc) = channel::<(SocketAddr, BinaryStream)>();
-
-          thread::spawn(move || {
-               loop {
-                    let (address, stream) = match rc.try_recv() {
-                         Ok(t) => t,
-                         Err(TryRecvError) => {
-                              println!("Could not recieve any data from mspc as channel is probably closed.");
-                              continue;
-                         }
-                    };
-                    let mut st = BinaryStream::init(&stream.get_buffer());
-
-                    if address.ip().is_loopback() && st.read_byte() == 0xCE && st.read_long() == THREAD_QUIT {
-                         println!("Recieved quit message");
-                         break;
-                    }
-
-                    res.as_ref().send_to(&*stream.get_buffer(), address).expect("Could not send bytes to client.");
-               }
-          });
+     fn start(&mut self) -> (&mut RakServer, Arc<RakEmitter>) {
+          return start(self);
      }
+}
+
+pub fn start(serv: &mut RakServer) -> (&mut RakServer, Arc<RakEmitter>) {
+     let socket = UdpSocket::bind(serv.address.parse::<SocketAddr>().unwrap());
+     let resource: Arc<UdpSocket> = Arc::new(socket.unwrap());
+     let res = Arc::clone(&resource);
+     let ev_channel = RakEmitter::new();
+     let ec1 = Arc::new(ev_channel);
+     let ec2 = Arc::clone(&ec1);
+
+     // ServerBound
+     thread::spawn(move || {
+          let mut buf = [0; 65535];
+          loop {
+               let (len, rem) = match resource.as_ref().recv_from(&mut buf) {
+                    Ok(v) => v,
+                    Err(_e) => {
+                         continue;
+                    }
+               };
+
+               let data = &buf[..len];
+               let ev = RakEv::Recieve(rem, BinaryStream::init(&data.to_vec()));
+               ec1.as_ref().broadcast(&ev);
+          }
+     });
+
+     // mspc channels recievers don't work on threads?
+     let (_sr, rc) = channel::<(SocketAddr, BinaryStream)>();
+
+     thread::spawn(move || {
+          loop {
+               let (address, stream) = match rc.try_recv() {
+                    Ok(t) => t,
+                    Err(_e) => {
+                         println!("Could not recieve any data from mspc as channel is probably closed.");
+                         continue;
+                    }
+               };
+               let mut st = BinaryStream::init(&stream.get_buffer());
+
+               if address.ip().is_loopback() && st.read_byte() == 0xCE && st.read_long() == THREAD_QUIT {
+                    println!("Recieved quit message");
+                    break;
+               }
+
+               res.as_ref().send_to(&*stream.get_buffer(), address).expect("Could not send bytes to client.");
+          }
+     });
+
+     return (serv, ec2);
 }
