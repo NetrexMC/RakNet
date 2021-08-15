bf13dcf0e42ae25752c435640fb41f323ba04cbd
Olybear9 <olybear9@gmail.com>
2021-08-14 23:12:42 -0500

    chore: Fix a bug with frame ordering

--- test/src/main.rs
+++ test/src/main.rs
@@ -4,11 +4,11 @@ use binary_utils::*;
 
 fn main() {
      let mut server = RakNetServer::new(String::from("0.0.0.0:19132"));
-     server.set_reciever(|con: &mut Connection, pk: &mut BinaryStream| {
-          println!("Got byte: {}", pk.read_byte());
+     server.set_reciever(|_con: &mut Connection, pk: &mut BinaryStream| {
+          println!("Got Final packet: {:?}", pk);
      });
      let threads = server.start();
-     threads.0.join().unwrap();
-     threads.1.join().unwrap();
-     println!("Hi I am running");
+     threads.0.join();
+     threads.1.join();
+     println!("Hi I am running concurrently.");
 }
