a57d982633b15bbadb3498898091720a2f04613f
Olybear9 <olybear9@gmail.com>
2022-01-08 17:52:38 -0600

    feat: Put test in tests/ directory

--- tests/generic.rs
+++ tests/generic.rs
@@ -2,18 +2,23 @@ use rakrs::Motd;
 use rakrs::RakEvent;
 use rakrs::RakNetServer;
 use rakrs::RakResult;
+use rakrs::start;
+use futures::Future;
+use tokio::runtime::Handle;
+use tokio::runtime::Runtime;
+
 
 #[test]
-pub fn test_server() {
-    let mut server = RakNetServer::new(String::from("0.0.0.0:19132"));
-    let mut motd = Motd {
+pub fn test_boot() {
+    let server = RakNetServer::new(String::from("0.0.0.0:19132"));
+    let motd = Motd {
         name: "Netrex Raknet".to_owned(),
         protocol: 190,
         player_count: 0,
         player_max: 10000,
         gamemode: "creative".to_owned(),
         version: "1.18.9".to_owned(),
-        server_id: 2747994720109207718 as i64,
+        server_id: 2747994720109207713 as i64,
     };
     let channel = netrex_events::Channel::<RakEvent, RakResult>::new();
     let mut unknown = 0;
@@ -33,13 +38,13 @@ pub fn test_server() {
         None
     };
     channel.receive(&mut listener);
-    server.start(channel);
-    
 
+    // this is a hack!
+    // get the tokio runtime and wait pulling!
     println!("Hi I am running concurrently.");
-
-    loop {
-        let result = server.motd.clone();
-        println!("Motd: {:?}", result);
-    }
+    let rt = Runtime::new().unwrap();
+    let handle = rt.handle();
+    handle.block_on(async move {
+        start(server, channel).await;
+    });
 }
