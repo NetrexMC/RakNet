38cb9f51e28662b38324e1004a30f31006935b6a
Bavfalcon9 <olybear9@gmail.com>
2023-01-14 12:35:30 -0600

    chore: Fix tests

--- src/connection/queue/mod.rs
+++ src/connection/queue/mod.rs
@@ -132,6 +132,30 @@ impl<Item> NetQueue<Item> for RecoveryQueue<Item> {
     }
 }
 
+/// An ordered queue is used to Index incoming packets over a channel
+/// within a reliable window time.
+///
+/// Usage:
+/// ```rust
+/// use rakrs::connection::queue::OrderedQueue;
+/// let mut ord_qu: OrderedQueue<Vec<u8>> = OrderedQueue::new();
+/// // Insert a packet with the id of "1"
+/// ord_qu.insert(1, vec![0, 1]);
+/// ord_qu.insert(5, vec![1, 0]);
+/// ord_qu.insert(3, vec![2, 0]);
+///
+/// // Get the packets we still need.
+/// let needed: Vec<u32> = ord_qu.missing();
+/// assert_eq!(needed, vec![0, 2, 4]);
+///
+/// // We would in theory, request these packets, but we're going to insert them
+/// ord_qu.insert(4, vec![2, 0, 0, 1]);
+/// ord_qu.insert(2, vec![1, 0, 0, 2]);
+///
+/// // Now let's return our packets in order.
+/// // Will return a vector of these packets in order by their "id".
+/// let ordered: Vec<Vec<u8>> = ord_qu.flush();
+/// ```
 #[derive(Debug, Clone)]
 pub struct OrderedQueue<Item: Clone + std::fmt::Debug> {
     /// The current ordered queue channels
@@ -211,129 +235,6 @@ where
     }
 }
 
-/// An ordered queue is used to Index incoming packets over a channel
-/// within a reliable window time.
-///
-/// Usage:
-/// ```rust
-/// use rakrs::conn::queue::OrderedQueue;
-/// let mut ord_qu: OrderedQueue<Vec<u8>> = OrderedQueue::new();
-/// // Insert a packet with the id of "1"
-/// ord_qu.insert(vec![0, 1], 1);
-/// ord_qu.insert(vec![1, 0], 5);
-/// ord_qu.insert(vec![2, 0], 3);
-///
-/// // Get the packets we still need.
-/// let needed: Vec<u32> = ord_qu.flush_missing();
-/// assert_eq!(needed, vec![0, 2, 4]);
-///
-/// // We would in theory, request these packets, but we're going to insert them
-/// ord_qu.insert(vec![2, 0, 0, 1], 4);
-/// ord_qu.insert(vec![1, 0, 0, 2], 2);
-///
-/// // Now let's return our packets in order.
-/// // Will return a vector of these packets in order by their "id".
-/// let ordered: Vec<Vec<u8>> = ord_qu.flush();
-/// ```
-#[derive(Debug)]
-pub struct OrderedQueueOld<T> {
-    /// The queue of packets that are in order. Mapped to the time they were received.
-    queue: HashMap<u32, T>,
-    /// The current starting scope for the queue.
-    /// A start scope or "window start" is the range of packets that we are currently allowing.
-    /// Older packets will be ignored simply because they are old.
-    scope: (u32, u32),
-}
-
-impl<T> Clone for OrderedQueueOld<T>
-where
-    T: Clone,
-{
-    fn clone(&self) -> Self {
-        OrderedQueueOld {
-            queue: self.queue.clone(),
-            scope: self.scope.clone(),
-        }
-    }
-}
-
-impl<T> OrderedQueueOld<T>
-where
-    T: Sized + Clone,
-{
-    pub fn new() -> Self {
-        Self {
-            queue: HashMap::new(),
-            scope: (0, 0),
-        }
-    }
-
-    /// Inserts the given packet into the queue.
-    /// This will return `false` if the packet is out of scope.
-    pub fn insert(&mut self, packet: T, id: u32) -> bool {
-        // if the packet id is lower than our scope, ignore it
-        // this packet is way to old for us to handle.
-        if id < self.scope.0 {
-            return false;
-        }
-
-        // If the packet is higher than our current scope, we need to adjust our scope.
-        // This is because we are now allowing packets that are newer than our current scope.
-        if id > self.scope.1 {
-            self.scope.1 = id + 1;
-        }
-
-        self.queue.insert(id, packet);
-        return true;
-    }
-
-    /// Drains the current queue by removing all packets from the queue.
-    /// This will return the packets in order only if they were within the current scope.
-    /// This method will also update the scope and adjust it to the newest window.
-    pub fn flush(&mut self) -> Vec<T> {
-        // clear all packets not within our scope
-        self.clear_out_of_scope();
-
-        // now drain the queue
-        let mut map = HashMap::new();
-        std::mem::swap(&mut map, &mut self.queue);
-
-        let mut clean = map.iter().collect::<Vec<_>>();
-        clean.sort_by_key(|m| m.0);
-
-        return clean.iter().map(|m| m.1.clone()).collect::<Vec<T>>();
-    }
-
-    /// Clears all packets that are out of scope.
-    /// Returning only the ones that have not been recieved.
-    pub fn flush_missing(&mut self) -> Vec<u32> {
-        let mut missing: Vec<u32> = Vec::new();
-        // we need to get the amount of ids that are missing from the queue.
-        for i in self.scope.0..self.scope.1 {
-            if !self.queue.contains_key(&i) {
-                missing.push(i);
-            }
-        }
-
-        // we can safely update the scope
-        self.scope.0 = missing.get(0).unwrap_or(&self.scope.0).clone();
-        return missing;
-    }
-
-    fn clear_out_of_scope(&mut self) {
-        // clear all packets not within our current scope.
-        // this is done by removing all packets that are older than our current scope.
-        for (id, _) in self.queue.clone().iter() {
-            if *id < self.scope.0 {
-                self.queue.remove(id);
-            }
-        }
-    }
-
-    pub fn get_scope(&self) -> u32 {
-        self.scope.1 - self.scope.0
-    }
-}
 
 /// A specialized structure for re-ordering fragments over the wire.
 /// You can use this structure to fragment frames as well.
